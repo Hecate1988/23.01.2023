3b78a7b893aa1cb61045f08654bc73483192d049
Admin <mesheryakova.mosk.syd@gmail.com>
2023-02-14 16:23:35 +0200

    second commit

--- lesson13.02.2023_dz20.py
+++ lesson13.02.2023_dz20.py
@@ -1,1 +1,17 @@
-def add_one(list):
+def add_one(nums):
+    if __name__ == '__main__':
+        s = ''.join(str(x) for x in nums)
+    string = str(int(s) + 1)
+    if __name__ == '__main__':
+      return list(map(int, string))
+
+print(add_one([1, 2, 3, 4]))
+print(add_one([9, 9, 9, 9]))
+print(add_one([0]))
+print(add_one([9]))
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9, 9]) == [1, 0, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
